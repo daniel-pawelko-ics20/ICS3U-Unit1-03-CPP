eff8ea15c193bc5f378daff61332ad619cbb736f
Ubuntu <ubuntu@ip-172-31-85-153.ec2.internal>
2021-10-04 23:13:29 +0000

    hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Daniel Pawelko All rights reserved
+//
+// Created by: Daniel Pawelko
+// Created on: Oct 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!";
+}
